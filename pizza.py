a5605418799567cc760ef03e7b0b60b75ad55ada
freez_meinster <freez_meinster@yahoo.co.id>
2014-04-12 07:52:50 +0700

    sampe get attribute

--- pizza.py
+++ pizza.py
@@ -18,34 +18,50 @@ class Pizza(object):
                 )
         result.pop()
         return result
-
-    def list_pool(self,detail=False,unit="kb"):
+        
+    def list_pool(self,detail=False,unit="gb"):
         if detail :
             list_pool = self.__list_pool()
             result = {}
             for pool in list_pool:
-                result[pool] = self.__get_capacity(pool,unit)
+                result[pool] = {
+                    'capacity' : self.__get_capacity(pool,unit),
+                    'datasheet' : self.__get_datasheet(pool)
+                }
             return result
             
         else :
             return self.__list_pool()
     
-    def __get_capacity(self,pool,unit):
+    def __get_capacity(self,datasheet,unit):
         used = execute_task(
-            ["zfs", "list", "-Hp -o", "used" ,pool],
+            ["zfs", "list", "-Hp" ,"-o", "used" ,datasheet],
             with_return=True,
-        )
+        ).pop()
         
         free = execute_task(
-            ["zfs", "list", "-Hp -o", "available" ,pool],
+            ["zfs", "list", "-Hp", "-o", "available" ,datasheet],
             with_return=True,
-        )
+        ).pop()
         
-        resutl = {
+        result = {
             'used' : convert_byte(used,unit),
             'free' : convert_byte(free,unit),
             'total' : convert_byte(used,unit) +
                       convert_byte(free,unit)
         }
         return result
-        
+    
+    def __get_datasheet(self,pool):
+        result = {}
+        raw = execute_task(
+            ['zfs', 'list','-Hp', '-o' ,'name' ,'-r', pool],
+            with_return=True,
+            spliter="new_line"
+        )
+        del raw[0]
+        del raw[-1]
+        return raw
+    
+    def __get_datasheet_attribute(self,datasheet,atrribute=all):
+        pass
--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 from pizza import Pizza
+from utils import execute_task
+
 
 p = Pizza()
 print p.list_pool(detail=True)
